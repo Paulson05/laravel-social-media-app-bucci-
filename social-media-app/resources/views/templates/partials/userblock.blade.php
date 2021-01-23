b5507b7739a4628645ee47aaa72a73f929a28431
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-01-23 15:12:16 +0100

    avater added

--- social-media-app/resources/views/templates/partials/userblock.blade.php
+++ social-media-app/resources/views/templates/partials/userblock.blade.php
@@ -1,7 +1,7 @@
 <div class="media" style="padding-top: 50px;">
     <a href="" class="pull-left">
 
-        <img src="" alt="" class="media-object">
+        <img src="{{ $user->getAvatarUrl() }}" alt="img" class="media-object">
     </a>
     <div class="media-body">
         <h4 class="media-heading"><a href="">{{ $user->first_name }}  {{$user->last_name }}</a></h4>
