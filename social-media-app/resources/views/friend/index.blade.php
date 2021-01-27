bdcb902649ba4627c5f6e89cf5ef6c973a695a95
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-01-27 09:05:22 +0100

    friend accepted functionality

--- social-media-app/resources/views/friend/index.blade.php
+++ social-media-app/resources/views/friend/index.blade.php
@@ -4,9 +4,9 @@
 <div class="row" style="margin-top:80px;">
     <div class="col-lg-6">
       <h3  style="margin:30px;">your Friends</h3>
-
+          
       @if(!$friends->count())
-      <p style="color: red;">your has no friends</p>
+    <p style="color: red;">your have no friends</p>
           @else
           @foreach($friends as $user)
           @include('/templates/partials/userblock')
--- social-media-app/resources/views/profile/index.blade.php
+++ social-media-app/resources/views/profile/index.blade.php
@@ -7,13 +7,22 @@
         <hr style="border: 2px solid blue">
     </div>
     <div class="col-lg-4 col-lg-6">
+        @if (Auth::user()->hasFriendRequestsPending($user))
+        <p>waiting for {{ $user->getNameOrUsername() }} to accept your request</p>
+    @elseif(Auth::user()->hasFriendRequestReceived($user))
+    
+        <a href="" class="btn btn-primary">Accept friend    Request</a>
+        @elseif(Auth::user()->isFriendsWith($user))
+        <p>you and  {{ $user->getNameOrUsername() }} are friends</p>
+        @endif
+        <h4>{{ $user->getFirstNameOrUsername() }} friends</h4>
     @if(!$user->friends()->count())
     <p style="color: red;">{{ $user->getFirstNameOrUsername() }} has no friends</p>
         @else
         @foreach($user->friends() as $user)
         @include('/templates/partials/userblock')
             
-        @endforeach
+        @endforeach 
     @endif
     
      </div>
