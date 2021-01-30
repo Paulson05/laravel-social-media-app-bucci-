65bbc3dad3a7e6f8e0233370889f54ef935e414f
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-01-30 08:10:22 +0100

    like model created and polymorphic relationship

---
+++ social-media-app/app/Models/Like.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{   
+    
+    // use HasFactory;
+    protected $table  = 'Likeable';
+
+public function likeable()
+{
+  
+return $this->morphTo();
+
+}
+
+ public function user()
+ {
+    $this->belongsTo('App\Models\Users',  'user_id');
+ }
+ 
+}
