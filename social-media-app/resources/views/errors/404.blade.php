399ae8be0d3b27bae5e53e28abde139465a671c6
onyebuchi okofu <onyebuchiokofu@gmail.com>
2021-02-03 19:55:39 +0100

    custom 404 page

---
+++ social-media-app/resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+
+@extends('templates.default')
+
+@section('content')
+ <div class="container" style="margin-top: 200px">
+    <p>pages could not b found</p>
+    <a href="{{ route('index') }}">GO Home</a>
+ </div>
+
+    
+@endsection
--- social-media-app/routes/web.php
+++ social-media-app/routes/web.php
@@ -2,16 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/alert', function () {
     return redirect()->route('index')->with('info', 'you are signed up!');
